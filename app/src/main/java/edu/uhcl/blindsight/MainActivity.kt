c3238c024fc133d2811869c4d6722e43970f9f93
Brett <brett@brettdoes.it>
2023-01-27 16:11:39 -0600

    UI functionality and Unit Testing for StringToDiceRoll

--- app/src/main/java/edu/uhcl/blindsight/MainActivity.kt
+++ app/src/main/java/edu/uhcl/blindsight/MainActivity.kt
@@ -2,6 +2,8 @@ package edu.uhcl.blindsight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
@@ -16,13 +18,18 @@ class MainActivity : AppCompatActivity() {
 
         val rolld20: ImageButton = findViewById(R.id.imageButton2)
         val rolld6: ImageButton = findViewById(R.id.imageButton3)
+        val stringRoller: Button = findViewById(R.id.button);
         val rollResults: TextView = findViewById(R.id.displayRoll)
+        val stringInput: EditText = findViewById(R.id.stringRoll)
 
         rolld20.setOnClickListener {
             rollResults.text = diceRoller.roll("1d20").toString()
         }
         rolld6.setOnClickListener {
             rollResults.text = diceRoller.roll("1d6").toString()
+        }
+        stringRoller.setOnClickListener {
+            rollResults.text = diceRoller.roll(stringInput.text.toString()).toString()
         }
         //A comment to test
     }
---
+++ app/src/test/java/edu/uhcl/blindsight/StringToDiceRollUnitTest.kt
@@ -0,0 +1,36 @@
+package edu.uhcl.blindsight
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class StringToDiceRollUnitTest {
+    private var diceRoller = DiceRoller()
+    @Test
+    fun multipleSameDice_isCorrect() {
+        assertEquals(true, diceRoller.roll("10d2") in 10..20)
+    }
+    @Test
+    fun multipleDifferentDice_isCorrect() {
+        assertEquals(true, diceRoller.roll("1d4+1d6+2d20") in 4 .. 50)
+    }
+    @Test
+    fun multiplyNumberOfDice_isCorrect() {
+        assertEquals(true, diceRoller.roll("(4*3)d4") in 12 .. 48)
+    }
+    @Test
+    fun addToDice_isCorrect() {
+        assertEquals(true, diceRoller.roll("1d2+30") in 31 .. 32)
+    }
+
+    @Test
+    fun subtractFromDice_isCorrect() {
+        assertEquals(true, diceRoller.roll("1d2-1") in 0 .. 1)
+    }
+
+}
