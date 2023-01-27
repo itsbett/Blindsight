2067b385e127c913a577ef562b2d67fe081a0b82
Brett <brett@brettdoes.it>
2023-01-27 14:39:05 -0600

    Added String -> Dice Roll Backend

--- app/src/main/java/edu/uhcl/blindsight/DiceRoller.java
+++ app/src/main/java/edu/uhcl/blindsight/DiceRoller.java
@@ -1,7 +1,10 @@
 package edu.uhcl.blindsight;
 
+import org.jetbrains.annotations.NotNull;
+
 public class DiceRoller {
-    public static int roll(String str) {
+    @NotNull
+    public int roll(@NotNull String str) {
         if (StringToDiceRollSyntax.hasCorrectSyntax(str)) { // checks syntax
             return StringToDiceRoll.eval(str); // Parses and computes mathematics
         } else {
--- app/src/main/java/edu/uhcl/blindsight/MainActivity.kt
+++ app/src/main/java/edu/uhcl/blindsight/MainActivity.kt
@@ -2,10 +2,27 @@ package edu.uhcl.blindsight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var diceRoller = DiceRoller()
+        val roll = diceRoller.roll("1d6")
         setContentView(R.layout.activity_main)
+
+        val rolld20: ImageButton = findViewById(R.id.imageButton2)
+        val rolld6: ImageButton = findViewById(R.id.imageButton3)
+        val rollResults: TextView = findViewById(R.id.displayRoll)
+
+        rolld20.setOnClickListener {
+            rollResults.text = diceRoller.roll("1d20").toString()
+        }
+        rolld6.setOnClickListener {
+            rollResults.text = diceRoller.roll("1d6").toString()
+        }
     }
 }
