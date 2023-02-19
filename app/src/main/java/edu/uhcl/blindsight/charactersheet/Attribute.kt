402cdb47bb94636f1723ece679f8d05ef9d3f84a
Brett <brett@brettdoes.it>
2023-02-19 12:55:18 -0600

    For Jason

---
+++ app/src/main/java/edu/uhcl/blindsight/charactersheet/Attribute.kt
@@ -0,0 +1,10 @@
+package edu.uhcl.blindsight.charactersheet
+
+class Attribute {
+    var name: String
+
+    constructor (name: String, value: Int, modifier: Int, additionalValues: Int) {
+        this.name = name
+
+    }
+}
