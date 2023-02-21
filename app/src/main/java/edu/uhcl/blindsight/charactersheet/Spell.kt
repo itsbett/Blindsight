ba26e37d59b761223431be42cb1766884b2372ce
Brett <brett@brettdoes.it>
2023-02-20 18:52:20 -0600

    Updating Spell Class and system

--- app/src/main/java/edu/uhcl/blindsight/charactersheet/Spell.kt
+++ app/src/main/java/edu/uhcl/blindsight/charactersheet/Spell.kt
@@ -1,29 +1,25 @@
 package edu.uhcl.blindsight.charactersheet
-class Spell {
-    var spellname: String
-    var school: String
-    var spellDescription: String
-    var DiceToRoll: String
-    var castingTime: String
-    var Duration: String
-    var range: String
-    var componets: String
-    var spellLevel: Int
-    var canUpcast: Boolean
-    var upcast: String
 
-    constructor (spellname: String, school: String, spellDescription: String, DiceToRoll: String, castingTime: String, Duration: String, range: String, componets: String, spellLevel: Int, canUpcast: Boolean, upcast: String) {
-        this.spellname = spellname
-        this.school = school
-        this.spellDescription = spellDescription
-        this.DiceToRoll = DiceToRoll
-        this.castingTime = castingTime
-        this.Duration = Duration
-        this.range = range
-        this.componets = componets
-        this.spellLevel = spellLevel
-        this.canUpcast = canUpcast
-        this.upcast = upcast
+data class SpellItem(
+    val casting_time: String,
+    val classes: List<String>,
+    val components: Components,
+    val description: String,
+    val duration: String,
+    val higher_levels: String,
+    val level: String,
+    val name: String,
+    val range: String,
+    val ritual: Boolean,
+    val school: String,
+    val tags: List<String>,
+    val type: String
+)
 
-    }
-}
+data class Components(
+    val material: Boolean,
+    val materials_needed: List<String>,
+    val raw: String,
+    val somatic: Boolean,
+    val verbal: Boolean
+)
