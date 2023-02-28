d441a34530bf032807a08ba4fc6f2396dee690ad
Brett <brett@brettdoes.it>
2023-02-28 13:58:14 -0600

    Added placeholder classes that need to be completed for CharacterSheet.

---
+++ app/src/main/java/edu/uhcl/blindsight/charactersheet/GameItem.kt
@@ -0,0 +1,7 @@
+package edu.uhcl.blindsight.charactersheet
+
+class GameItem {
+    /*
+    @TODO: Implement this class. This should represent any in-game item that can be placed in inventory.
+     */
+}
---
+++ app/src/main/java/edu/uhcl/blindsight/charactersheet/LevelBenefits.kt
@@ -0,0 +1,9 @@
+package edu.uhcl.blindsight.charactersheet
+
+class LevelBenefits {
+    /*
+    @TODO: Create a class that contains the benefits of a single level from a class.
+    * e.g. this will contain "Choose Fighting Style" and "Second Wind" for the 1st level
+    * of fighter.
+     */
+}
