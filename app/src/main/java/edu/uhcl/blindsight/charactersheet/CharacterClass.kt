2266ff828a456379618d49ff588500a9ed60dfaf
Brett <brett@brettdoes.it>
2023-02-28 13:59:43 -0600

    Beginning CharacterSheet and Character Creation

--- app/src/main/java/edu/uhcl/blindsight/charactersheet/CharacterClass.kt
+++ app/src/main/java/edu/uhcl/blindsight/charactersheet/CharacterClass.kt
@@ -5,5 +5,5 @@ data class CharacterClass (
     var classHP: String = "1d10",
     var proficiency: Map<String, String> = emptyMap<String, String>(),
     var equipmentChoices: List<GameItem> = emptyList(),
-    var levelUpSheet: List<LevelBenefits>
+    var levelUpSheet: List<LevelBenefits> = emptyList()
     )
