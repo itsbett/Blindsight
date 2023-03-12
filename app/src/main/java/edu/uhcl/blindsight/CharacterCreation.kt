59f9c91739d84d94340ce4197f9a5c4cee61d486
Jeremy <jeremyfogleman@gmail.com>
2023-03-12 17:04:31 -0500

    Added Character Creation Activity

--- app/src/main/java/edu/uhcl/blindsight/CharacterCreation.kt
+++ app/src/main/java/edu/uhcl/blindsight/CharacterCreation.kt
@@ -19,48 +19,34 @@ import androidx.core.content.ContextCompat
 import edu.uhcl.blindsight.diceroller.DiceRoller
 import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity(), RecognitionListener,TextToSpeech.OnInitListener {
+class CharacterCreation : AppCompatActivity(), RecognitionListener, TextToSpeech.OnInitListener {
 
-    //Initializing variables to be used by Speech Recognizer functions
     private val permissionCode = 100
     private lateinit var voiceResults: TextView
-    private lateinit var commandTextBox: TextView
     private lateinit var micButton: ImageView
     private lateinit var speechRecognizer: SpeechRecognizer
     private lateinit var recognizerIntent: Intent
     private var tts: TextToSpeech? = null
 
 
-
-    @SuppressLint("ClickableViewAccessibility")
+    @SuppressLint("MissingInflatedId", "ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val diceRoller = DiceRoller()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_character_creation)
 
-        //Speech Recognizer elements
         voiceResults = findViewById(R.id.displayVoiceText)
-        commandTextBox = findViewById(R.id.commandTest)
         micButton = findViewById(R.id.micButton)
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
 
         tts = TextToSpeech(this,this)
 
-        //Create listener and intent for Speech Recognizer
         speechRecognizer.setRecognitionListener(this)
         recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "US-en")
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
         recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3)
-        //recognizerIntent.putExtra(RecognizerIntent.EXTRA_BIASING_STRINGS) Not available in API 24...kill me...
 
-        //Check for permissions: If microphone permissions are not granted, ask for permission
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            val permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
-            ActivityCompat.requestPermissions(this, permissions, 0)
-        }
-
-        //Mic button
         micButton.setOnTouchListener { _, motionEvent ->
             if (motionEvent.action == MotionEvent.ACTION_UP) {
                 speechRecognizer.stopListening()
@@ -73,9 +59,6 @@ class MainActivity : AppCompatActivity(), RecognitionListener,TextToSpeech.OnIni
         }
     }
 
-    //This method is invoked after .requestPermission is called
-    //However, it does not seem to work properly
-    //Work in progress
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissionArray: Array<out String>,
@@ -84,11 +67,11 @@ class MainActivity : AppCompatActivity(), RecognitionListener,TextToSpeech.OnIni
         super.onRequestPermissionsResult(requestCode, permissionArray, grantResults)
         when (requestCode) {
             permissionCode -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this@MainActivity, "Permission Granted",
-                Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@CharacterCreation, "Permission Granted",
+                    Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this@MainActivity, "Permission Denied",
-                Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@CharacterCreation, "Permission Denied",
+                    Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -99,9 +82,7 @@ class MainActivity : AppCompatActivity(), RecognitionListener,TextToSpeech.OnIni
         val matchedText: String = matches!![0]
         voiceResults.text = matchedText
         micButton.setImageResource(R.drawable.ic_mic_black_off)
-        val commandTest: String = TextToFunction.readText(matchedText)
-        commandTextBox.text = commandTest
-        ttsSpeak(commandTest)
+
 
     }
 
@@ -130,6 +111,7 @@ class MainActivity : AppCompatActivity(), RecognitionListener,TextToSpeech.OnIni
             }
         }
     }
+
     private fun ttsSpeak(textToSpeak: String) {
         tts!!.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null, "")
     }
