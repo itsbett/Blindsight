19dedb664816af0283c7db53f1f9e49b6017e27f
itsbett <langfordbrett@gmail.com>
2023-03-14 10:20:23 -0500

    Added more character creation steps

--- app/src/main/java/edu/uhcl/blindsight/CharacterCreationTextToFunction.kt
+++ app/src/main/java/edu/uhcl/blindsight/CharacterCreationTextToFunction.kt
@@ -1,79 +1,106 @@
 package edu.uhcl.blindsight
 
 import edu.uhcl.blindsight.charactersheet.CharacterManager
-import edu.uhcl.blindsight.diceroller.StringToDiceRoll
 import java.util.*
 
-class CharacterCreationTextToFunction {
-    private var currentStep: Int = 0
-    private lateinit var characterManager: CharacterManager
+class CharacterCreationTextToFunction(private var characterManager: CharacterManager) {
+    private var currentStep: String = "CHOOSE_RACE"
 
-    constructor(characterManager: CharacterManager) {
-        this.characterManager = characterManager
-    }
-
-    fun wordList(line: String): Array<String?>? {
+    private fun wordList(line: String): Array<String?> {
         return line.split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
     }
 
-    fun readText(input: String): String? {
+    fun readText(input: String): String {
         var inputString = input
         inputString = inputString.uppercase(Locale.getDefault()) //Converts the string to all uppercase for better recognition
-        val inputStringArray = TextToFunction.wordList(inputString) //Assigns inputString into a String array
+        val inputStringArray = wordList(inputString) //Assigns inputString into a String array
         return when(currentStep) {
 
-            //CHOOSE RACE
-            0 ->
+            "CHOOSE_RACE" ->
             {
-                if (characterManager.setRace(inputStringArray[0]) == 0) {
-                    this.currentStep++
+                if (inputStringArray[0]?.let { characterManager.setRace(it) } == 0) {
+                    this.currentStep = "CHOOSE_CLASS"
                     return "Race set to $inputStringArray[0]"
                 } else {
                     return "Race not found."
                 }
             }
 
-            //CHOOSE CLASS
-            1 ->
+            "CHOOSE_HEIGHT" ->
             {
-                if (characterManager.setClass(inputStringArray[0]) == 0) {
-                    this.currentStep++
-                    return "Class set to $inputStringArray[0]"
-                } else {
-                    return "Class not found."
-                }
-            }
-
-            //CHOOSE HEIGHT
-            2 ->
-            {
-                if (characterManager.setHeight(inputStringArray[0]) == 0) {
-                    this.currentStep++
+                if (inputStringArray[0]?.let { characterManager.setHeight(it) } == 0) {
+                    this.currentStep = "CHOOSE_WEIGHT"
                     return "Height set to $inputStringArray[0]"
                 } else {
                     return "Height could not be set"
                 }
             }
 
-            //CHOOSE WEIGHT
-            3 ->
+            "CHOOSE_WEIGHT" ->
             {
-                if (characterManager.setWeight(inputStringArray[0]) == 0) {
-                    this.currentStep++
+                if (inputStringArray[0]?.let { characterManager.setWeight(it) } == 0) {
+                    this.currentStep = "CHOOSE_NAME"
                     return "Weight set to $inputStringArray[0]"
                 } else {
                     return "Weight could not be set."
                 }
             }
 
-            4 ->
+            "CHOOSE_NAME" ->
             {
-                if (characterManager.setClass(inputStringArray[0]) == 0) {
-                    this.currentStep++
+                if (inputStringArray[0]?.let { characterManager.setClass(it) } == 0) {
+                    this.currentStep = "CHOOSE_GENDER"
                     return "Class set to $inputStringArray[0]"
                 } else {
                     return "Class not found."
                 }
+            }
+            "CHOOSE_GENDER" ->
+            {
+                return "Placeholder"
+            }
+            "CHOOSE_PHYSICAL_DESCRIPTION" ->
+            {
+                return "Placeholder"
+            }
+            "CHOOSE_BACKGROUND" ->
+            {
+                return "Placeholder"
+            }
+            "CHOOSE_ALIGNMENT" ->
+            {
+                return "Placeholder"
+            }
+            "CHOOSE_ABILITY_SCORE_METHOD" ->
+            {
+                return "Placeholder"
+            }
+            "ALLOCATE_ABILITY_SCORES" ->
+            {
+                return "Placeholder"
+            }
+            "ALLOCATE_ABILITY_SCORES_CUSTOM" ->
+            {
+                //ToDo: Calculate modifiers; set Initiative to Dex Mod
+                return "Placeholder"
+            }
+            "CHOOSE_CLASS" ->
+            {
+                if (inputStringArray[0]?.let { characterManager.setClass(it) } == 0) {
+                    this.currentStep = "CHOOSE_HEIGHT"
+                    return "Class set to $inputStringArray[0]"
+                } else {
+                    return "Class not found."
+                }
+            }
+            "CHOOSE_STARTING_EQUIPMENT" ->
+            {
+                return "Placeholder"
+            }
+            "SAVE_CHARACTER" ->
+            {
+                //ToDo: Update saving throws; update proficiencies; set armor class
+                return "Placeholder"
             }
             else -> "error"
         }
