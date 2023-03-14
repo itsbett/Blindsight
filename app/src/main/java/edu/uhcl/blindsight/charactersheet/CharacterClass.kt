de31a614e910ed855416f9087f50d4bab639d9a0
itsbett <langfordbrett@gmail.com>
2023-03-14 13:02:35 -0500

    Added starting gold value

--- app/src/main/java/edu/uhcl/blindsight/charactersheet/CharacterClass.kt
+++ app/src/main/java/edu/uhcl/blindsight/charactersheet/CharacterClass.kt
@@ -3,6 +3,7 @@ package edu.uhcl.blindsight.charactersheet
 data class CharacterClass (
     var className: String = "Fighter",
     var classHP: String = "1d10",
+    var startingGold: String = "5d4*10",
     var proficiency: Map<String, String> = emptyMap<String, String>(),
     //var equipmentChoices: List<GameItem> = emptyList(),
     var equipmentChoices: Array<Array<String>> = emptyArray(),
