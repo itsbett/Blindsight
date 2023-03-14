43ba0a545933f24f6f28739bac3a7690b652b27a
itsbett <langfordbrett@gmail.com>
2023-03-14 09:40:40 -0500

    Modified to have default values

--- app/src/main/java/edu/uhcl/blindsight/charactersheet/CharacterRace.kt
+++ app/src/main/java/edu/uhcl/blindsight/charactersheet/CharacterRace.kt
@@ -3,10 +3,10 @@ package edu.uhcl.blindsight.charactersheet
 data class CharacterRace(
     var raceName: String = "",
     var abilityBonuses: ArrayList<String> = ArrayList<String>(),
-    var speed: Int,
-    var size: Char,
-    var spellAbility: String,
-    var featAtCreation: Boolean,
-    var proficiencyAtCreation: String,
+    var speed: Int = 0,
+    var size: Char = '0',
+    var spellAbility: String = "",
+    var featAtCreation: Boolean = false,
+    var proficiencyAtCreation: String = "",
     var racialTraits: ArrayList<String> = ArrayList<String>()
 )
